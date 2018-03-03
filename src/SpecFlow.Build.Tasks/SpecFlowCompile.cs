30156fab282db8b82cb2fd9705fac9404d17abda
ChristopherHaws <cribs2@gmail.com>
2018-03-02 20:55:14 -0800

    Add project files.

---
+++ src/SpecFlow.Build.Tasks/SpecFlowCompile.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using TechTalk.SpecFlow.Configuration;
+using TechTalk.SpecFlow.Generator;
+using TechTalk.SpecFlow.Generator.Interfaces;
+using TechTalk.SpecFlow.Generator.Project;
+using TechTalk.SpecFlow.Plugins;
+using TechTalk.SpecFlow.Tracing;
+using TechTalk.SpecFlow.Utils;
+
+namespace SpecFlow.Build.Tasks
+{
+	public class SpecFlowCompile : SpecFlowTask
+	{
+		private readonly List<ITaskItem> generatedFiles = new List<ITaskItem>();
+
+		public Boolean VerboseOutput { get; set; }
+
+		[Required]
+		public String ProjectPath { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of feature files being transpiled.
+		/// </summary>
+		[Required]
+		public ITaskItem[] FeatureFiles { get; set; }
+
+		/// <summary>
+		/// Gets the generated feature files that have had been transpiled.
+		/// </summary>
+		[Output]
+		public ITaskItem[] GeneratedFiles => this.generatedFiles.ToArray();
+
+		protected override void DoExecute()
+		{
+			//System.Diagnostics.Debugger.Launch();
+
+			var traceListener = this.VerboseOutput
+				? (TechTalk.SpecFlow.Tracing.ITraceListener)new TechTalk.SpecFlow.Tracing.TextWriterTraceListener(GetMessageWriter(MessageImportance.High), "SpecFlow: ")
+				: new NullListener();
+
+			var specFlowProject = MsBuildProjectReader.LoadSpecFlowProjectFromMsBuild(this.ProjectPath);
+			specFlowProject.ProjectSettings.ConfigurationHolder = new SpecFlowConfigurationHolder(
+				specFlowProject.ProjectSettings.ConfigurationHolder.ConfigSource,
+				Environment.ExpandEnvironmentVariables(specFlowProject.ProjectSettings.ConfigurationHolder.Content)
+			);
+
+			FixPluginPaths(specFlowProject.Configuration.SpecFlowConfiguration);
+
+			traceListener.WriteToolOutput("Processing project: " + specFlowProject.ProjectSettings.ProjectName);
+			var generationSettings = new GenerationSettings
+			{
+				CheckUpToDate = false,
+				WriteResultToFile = true
+			};
+
+			using (var container = GeneratorContainerBuilder.CreateContainer(specFlowProject.ProjectSettings.ConfigurationHolder, specFlowProject.ProjectSettings))
+			using (var generator = container.Resolve<ITestGenerator>())
+			{
+				traceListener.WriteToolOutput("Using Generator: {0}", generator.GetType().FullName);
+
+				var featureFiles = this.FeatureFiles
+					.Select(x => new FeatureFileInput(FileSystemHelper.GetRelativePath(x.ItemSpec, specFlowProject.ProjectSettings.ProjectFolder)))
+					.ToList();
+
+				foreach (var featureFile in featureFiles)
+				{
+					var outputFilePath = generator.GetTestFullPath(featureFile);
+					featureFile.GeneratedTestProjectRelativePath = FileSystemHelper.GetRelativePath(outputFilePath, specFlowProject.ProjectSettings.ProjectFolder);
+
+					var generationResult = generator.GenerateTestFile(featureFile, generationSettings);
+					if (!generationResult.Success)
+					{
+						traceListener.WriteToolOutput("{0} -> test generation failed", featureFile.ProjectRelativePath);
+
+						foreach (var testGenerationError in generationResult.Errors)
+						{
+							this.RecordError(testGenerationError.Message, featureFile.GetFullPath(specFlowProject.ProjectSettings), testGenerationError.Line, testGenerationError.LinePosition);
+						}
+					}
+					else if (generationResult.IsUpToDate)
+					{
+						traceListener.WriteToolOutput("{0} -> test up-to-date", featureFile.ProjectRelativePath);
+					}
+					else
+					{
+						traceListener.WriteToolOutput("{0} -> test updated", featureFile.ProjectRelativePath);
+					}
+
+					if (generationResult.Success)
+					{
+						this.generatedFiles.Add(new TaskItem(featureFile.GetGeneratedTestFullPath(specFlowProject.ProjectSettings)));
+					}
+				}
+			}
+		}
+
+		private static void FixPluginPaths(SpecFlowConfiguration configuration)
+		{
+			var plugins = new List<PluginDescriptor>();
+
+			foreach (var plugin in configuration.Plugins)
+			{
+				var path = plugin.Path;
+				if (path != null)
+				{
+					path = Environment.ExpandEnvironmentVariables(path);
+				}
+
+				plugins.Add(new PluginDescriptor(plugin.Name, path, plugin.Type, plugin.Parameters));
+			}
+
+			configuration.Plugins = plugins;
+		}
+	}
+}
---
+++ src/SpecFlow.Build.Tasks/SpecFlowTask.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace SpecFlow.Build.Tasks
+{
+	public abstract class SpecFlowTask : Task
+	{
+		static SpecFlowTask()
+		{
+			AssemblyResolver.Enable();
+		}
+
+		public Boolean ShowTrace { get; set; }
+
+		protected internal CompilerErrorCollection Errors { get; private set; }
+
+		public override Boolean Execute()
+		{
+			this.Errors = new CompilerErrorCollection();
+
+			try
+			{
+				DoExecute();
+			}
+			catch (Exception ex)
+			{
+				RecordException(ex);
+			}
+
+			// handle errors
+			if (this.Errors.Count > 0)
+			{
+				var hasErrors = false;
+				foreach (CompilerError error in this.Errors)
+				{
+					if (error.IsWarning)
+						OutputWarning(error.ToString(), error.ErrorText, error.FileName, error.Line, error.Column);
+					else
+					{
+						OutputError(error.ToString(), error.ErrorText, error.FileName, error.Line, error.Column);
+						hasErrors = true;
+					}
+				}
+
+				return !hasErrors;
+			}
+
+			return true;
+		}
+
+		public void RecordException(Exception ex)
+		{
+			var message = ex.Message;
+			if (ShowTrace)
+				message += Environment.NewLine + ex;
+			this.Errors.Add(new CompilerError(String.Empty, 0, 0, null, message));
+		}
+
+		public void RecordError(string message, string fileName, int lineNumber, int columnNumber)
+		{
+			this.Errors.Add(new CompilerError(fileName, lineNumber, columnNumber, null, message));
+		}
+
+		protected void OutputError(string outString, string message, string fileName, int lineNumber, int columnNumber)
+		{
+			message = message.TrimEnd('\n', '\r');
+
+			Log.LogError(null, null, null, fileName, lineNumber, columnNumber, 0, 0, message);
+		}
+
+		protected void OutputWarning(string outString, string message, string fileName, int lineNumber, int columnNumber)
+		{
+			message = message.TrimEnd('\n', '\r');
+
+			Log.LogWarning(null, null, null, fileName, lineNumber, columnNumber, 0, 0, message);
+		}
+
+		protected void OutputInformation(MessageImportance importance, string message, params object[] messageArgs)
+		{
+			message = message.TrimEnd('\n', '\r');
+
+			Log.LogMessage(importance, message, messageArgs);
+		}
+
+		private class MessageTextWriter : TextWriter
+		{
+			private SpecFlowTask task;
+			private MessageImportance importance = MessageImportance.Normal;
+
+			public MessageTextWriter(SpecFlowTask task)
+			{
+				this.task = task;
+			}
+
+			public MessageTextWriter(SpecFlowTask task, MessageImportance importance)
+			{
+				this.task = task;
+				this.importance = importance;
+			}
+
+			public override Encoding Encoding => Encoding.Unicode;
+
+			public override void Write(char value)
+			{
+				Write(value.ToString());
+			}
+
+			public override void Write(char[] buffer, int index, int count)
+			{
+				Write(new string(buffer, index, count));
+			}
+
+			public override void Write(string value)
+			{
+				task.OutputInformation(importance, value);
+			}
+		}
+
+		protected TextWriter GetMessageWriter()
+		{
+			return new MessageTextWriter(this);
+		}
+
+		protected TextWriter GetMessageWriter(MessageImportance importance)
+		{
+			return new MessageTextWriter(this, importance);
+		}
+
+		protected abstract void DoExecute();
+	}
+}
